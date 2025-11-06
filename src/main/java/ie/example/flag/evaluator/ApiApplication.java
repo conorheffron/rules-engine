280562f0d0e5c1d7d03b01f934824520d42e7ba4
Conor H <conor.heffron@gmail.com>
2025-11-06 01:52:57 +0000

    Upload bpmn with Diagram, Resolve bug for loading embedded Camunda UI tools

--- src/main/java/ie/example/flag/evaluator/ApiApplication.java
+++ src/main/java/ie/example/flag/evaluator/ApiApplication.java
@@ -1,11 +1,27 @@
 package ie.example.flag.evaluator;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class ApiApplication {
-    static void main(String[] args) {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    public static void main(String... args) {
         SpringApplication.run(ApiApplication.class, args);
     }
+
+    @EventListener
+    public void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+
 }
